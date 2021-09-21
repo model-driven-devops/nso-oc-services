7315199fc8ebaf2e4ebea7c6b4dc1b42070da30f
stmosher <stmosher@cisco.com>
2021-09-21 09:07:00 -0400

    added bgp ipv4 neighbors added tests

--- mdd/python/translation/openconfig_xe/xe_bgp.py
+++ mdd/python/translation/openconfig_xe/xe_bgp.py
@@ -16,19 +16,14 @@ def xe_bgp_global_program_service(self) -> None:
         device_bgp_cbd.distance.bgp.local = '200'  # TODO add this to extensions
 
     if self.service.graceful_restart:
-        self.log.info(f'Vlaue {self.service.graceful_restart.config.restart_time}')
-        self.log.info(f'Type {type(self.service.graceful_restart.config.restart_time)}')
-        self.log.info(f'Vlaue {self.service.graceful_restart.config.stale_routes_time}')
-        self.log.info(f'Type {type(self.service.graceful_restart.config.stale_routes_time)}')
-        self.log.info(f'Vlaue {self.service.graceful_restart.config.enabled}')
-        self.log.info(f'Type {type(self.service.graceful_restart.config.enabled)}')
         if self.service.graceful_restart.config.enabled:
             if not device_bgp_cbd.bgp.graceful_restart.exists():
                 device_bgp_cbd.bgp.graceful_restart.create()
             if self.service.graceful_restart.config.restart_time:
                 device_bgp_cbd.bgp.graceful_restart_conf.graceful_restart.restart_time = self.service.graceful_restart.config.restart_time
             if self.service.graceful_restart.config.stale_routes_time:
-                device_bgp_cbd.bgp.graceful_restart_conf.graceful_restart.stalepath_time = int(float(self.service.graceful_restart.config.stale_routes_time))
+                device_bgp_cbd.bgp.graceful_restart_conf.graceful_restart.stalepath_time = int(
+                    float(self.service.graceful_restart.config.stale_routes_time))
 
     if self.service.route_selection_options:
         if self.service.route_selection_options.config.always_compare_med:
@@ -53,8 +48,60 @@ def xe_bgp_neighbor_program_service(self) -> None:
     """
     Program service for xe NED features
     """
-    self.log.info(f'SELF SERVICE  {self.service}')
-    self.log.info(f'DIR SELF SERVICE {dir(self.service)}')
+    asn = ''
+    for i in self.root.devices.device[self.device_name].config.ios__router.bgp:
+        asn = i.as_no
+        break
+    if asn:
+        if self.service.neighbor_address and self.service.config.peer_as:
+            if not self.root.devices.device[self.device_name].config.ios__router.bgp[asn].neighbor.exists(self.service.neighbor_address):
+                self.root.devices.device[self.device_name].config.ios__router.bgp[asn].neighbor.create(self.service.neighbor_address)
+            neighbor = self.root.devices.device[self.device_name].config.ios__router.bgp[asn].neighbor[self.service.neighbor_address]
+            neighbor.remote_as = self.service.config.peer_as
+            if self.service.config:
+                if self.service.config.auth_password:
+                    neighbor.password.text = self.service.config.auth_password
+                if self.service.config.description:
+                    neighbor.description = self.service.config.description
+                if not self.service.config.enabled:
+                    neighbor.shutdown.create()
+                if self.service.config.local_as:
+                    neighbor.local_as.create()
+                    neighbor.local_as.as_no = self.service.config.local_as
+                if self.service.config.remove_private_as:
+                    neighbor.remove_private_as.create()
+                    if self.service.config.remove_private_as == 'oc-bgp-types:PRIVATE_AS_REMOVE_ALL':
+                        neighbor.remove_private_as.all.create()
+                    elif self.service.config.remove_private_as == 'oc-bgp-types:PRIVATE_AS_REPLACE_ALL':
+                        neighbor.remove_private_as.all.create()
+                        neighbor.remove_private_as.replace_as.create()
+                if self.service.config.send_community and self.service.config.send_community != 'NONE':
+                    neighbor.send_community.create()
+                    if self.service.config.send_community == 'STANDARD':
+                        neighbor.send_community.send_community_where = 'standard'
+                    elif self.service.config.send_community == 'EXTENDED':
+                        neighbor.send_community.send_community_where = 'extended'
+                    elif self.service.config.send_community == 'BOTH':
+                        neighbor.send_community.send_community_where = 'both'
+            if self.service.ebgp_multihop:
+                if self.service.ebgp_multihop.config.enabled and self.service.ebgp_multihop.config.multihop_ttl:
+                    neighbor.ebgp_multihop.create()
+                    neighbor.ebgp_multihop.max_hop = self.service.ebgp_multihop.config.multihop_ttl
+            if self.service.route_reflector:
+                if self.service.route_reflector.config.route_reflector_client:
+                    neighbor.route_reflector_client.create()
+                if self.service.route_reflector.config.route_reflector_cluster_id:
+                    neighbor.cluster_id = self.service.route_reflector.config.route_reflector_cluster_id
+            if self.service.timers:
+                if self.service.timers.config.hold_time and self.service.timers.config.keepalive_interval:
+                    neighbor.timers.holdtime = int(float(self.service.timers.config.hold_time))
+                    neighbor.timers.keepalive_interval = int(float(self.service.timers.config.keepalive_interval))
+            if self.service.transport:
+                if not self.service.transport.config.mtu_discovery:
+                    neighbor.transport.path_mtu_discovery.create()
+                    neighbor.transport.path_mtu_discovery.disable.create()
+                if self.service.transport.config.passive_mode:
+                    neighbor.transport.connection_mode = 'passive'
 
 
 def xe_bgp_peergroup_program_service(self) -> None:
