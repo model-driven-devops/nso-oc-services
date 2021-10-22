a5f9c3f067b2a0c0e42a732f29380f8e74e10321
stmosher <stmosher@cisco.com>
2021-10-22 14:50:46 -0400

    - added xe expanded community-list support

--- mdd/python/translation/openconfig_xe/xe_routing_policy.py
+++ mdd/python/translation/openconfig_xe/xe_routing_policy.py
@@ -1,7 +1,9 @@
 # -*- mode: python; python-indent: 4 -*-
 import re
+import copy
 
 regex_ipv4_masklength_range = re.compile(r'([0-9]{1,2})\.\.([0-9]{1,2})')
+regex_meta = {'[', '\\', '.', '^', '$', '*', '+', '?', '{', '|', '('}
 
 
 def xe_routing_policy_program_service(self) -> None:
@@ -50,16 +52,42 @@ def as_path_sets_configure(self) -> None:
 
 def community_sets_configure(self) -> None:
     device = self.root.devices.device[self.device_name].config
-
     # Always use ip bgp-community new-format
     if not device.ios__ip.bgp_community.new_format.exists():
         device.ios__ip.bgp_community.new_format.create()
 
+    requested_community_lists = list()
     for service_community_set in self.service.oc_rpol__routing_policy.defined_sets.oc_bgp_pol__bgp_defined_sets.community_sets.community_set:
-        if not device.ios__ip.community_list.standard.exists(service_community_set.config.community_set_name):
-            device.ios__ip.community_list.standard.create(service_community_set.config.community_set_name)
+        temp_dict = {'name': service_community_set.config.community_set_name,
+                     'match-set-options': service_community_set.config.match_set_options,
+                     'list_type': 'standard',
+                     'communities': []}
+        for community in service_community_set.config.community_member:
+            if set(community) & regex_meta:  # check intersection of regex characters and community string
+                temp_dict['list_type'] = 'expanded'
+            if community == 'NO_EXPORT':
+                temp_dict['communities'].append('no-export')
+            elif community == 'NO_ADVERTISE':
+                temp_dict['communities'].append('no-advertise')
+            elif community == 'NO_EXPORT_SUBCONFED':
+                temp_dict['communities'].append('local-as')
+            elif community == 'NOPEER':
+                raise ValueError('OC BGP COMMUNITY NOPEER is not supported in Cisco IOS XE')
+            else:
+                temp_dict['communities'].append(community)
+        requested_community_lists.append(copy.deepcopy(temp_dict))
 
-        community_list_cdb = device.ios__ip.community_list.standard[service_community_set.config.community_set_name]
+    for request in requested_community_lists:
+        if request['list_type'] == 'standard':
+            if not device.ios__ip.community_list.standard.exists(request['name']):
+                device.ios__ip.community_list.standard.create(request['name'])
+            community_list_cdb = device.ios__ip.community_list.standard[request['name']]
+            for community_member in request['communities']:
+                community_list_cdb.entry.create(f'permit {community_member}')
 
-        for community_member in service_community_set.config.community_member:
-            community_list_cdb.entry.create(f'permit {community_member}')
+        elif request['list_type'] == 'expanded':
+            if not device.ios__ip.community_list.expanded.exists(request['name']):
+                device.ios__ip.community_list.expanded.create(request['name'])
+            community_list_cdb = device.ios__ip.community_list.expanded[request['name']]
+            for community_member in request['communities']:
+                community_list_cdb.entry.create(f'permit {community_member}')
