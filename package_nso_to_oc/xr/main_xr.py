a093f45441a0a44eb94ac85d5dd0e3714fcf5572
stmosher <stmosher@cisco.com>
2022-12-06 13:55:43 -0500

    feat: XR ACLs

--- package_nso_to_oc/xr/main_xr.py
+++ package_nso_to_oc/xr/main_xr.py
@@ -6,10 +6,13 @@ This module should only ever be called by main.py
 
 from xr import xr_system
 from xr import xr_interfaces
+from xr import xr_acls
 
 
 def build_xr_to_oc(config_before_dict, configs_leftover, oc, translation_notes: list):
     openconfig_interfaces = xr_interfaces.main(config_before_dict, configs_leftover, translation_notes)
     openconfig_system = xr_system.main(config_before_dict, configs_leftover, translation_notes)
+    openconfig_acls = xr_acls.main(config_before_dict, configs_leftover, translation_notes)
     oc['mdd:openconfig'].update(openconfig_system)
     oc['mdd:openconfig'].update(openconfig_interfaces)
+    oc['mdd:openconfig'].update(openconfig_acls)
